f9ca1ff636b7c2250be3f3514af8e6ccede34208
ruslanvs <ruslanvs@yahoo.com>
2018-02-09 17:33:24 -0800

    Assignment complete

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser');
+var path = require("path");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render( "index" );
+})
+
+var server = app.listen( 8000, function() {
+    console.log( "listening on port 8000" );
+});
+
+var io = require( "socket.io" ).listen( server );
+
+if ( typeof counter == "undefined" ) {
+    var counter = 0;
+}
+
+io.sockets.on( "connection", function( socket ){
+    console.log( "Client/socket is connected")
+    console.log( "Client/socket id is: ", socket.id );
+
+    socket.on( "counter_increment", function( data ){
+        counter ++;
+        io.emit( "counter", counter );
+    })
+
+    socket.on( "counter_reset", function( data ){
+        counter = 0;
+        io.emit( "counter", counter );
+    })
+})
